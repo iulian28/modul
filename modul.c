72640f72e43338f83176519a1b8d2f976772c896
Iulian <iulian20161@gmail.com>
2016-07-18 15:35:27 +0300

    Modul.c coding style

--- modul.c
+++ modul.c
@@ -1,6 +1,5 @@
 #include <linux/init.h>
 #include <linux/module.h>
-
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int __init hello_init(void)
@@ -8,12 +7,10 @@ static int __init hello_init(void)
 	pr_debug("Hello World!");
 	return 0;
 }
-
 static void __exit hello_exit(void)
 {
 	pr_debug("Bye!\n");
 }
-
 module_init(hello_init);
 module_exit(hello_exit);
 
