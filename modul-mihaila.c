fcb9f9b6210b44883c57d5db23b4cf35c0b6e59f
Iulian <iulian20161@gmail.com>
2016-07-18 21:53:12 +0300

    modul-mihaila.c

--- modul-mihaila.c
+++ modul-mihaila.c
@@ -1,16 +1,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
+
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int __init hello_init(void)
 {
-    printk(KERN_DEBUG "Hello, world\n");
-    return 0;
+	pr_debun("Hello, world\n");
+	return 0;
 }
+
 static void __exit hello_exit(void)
 {
-    printk(KERN_DEBUG "Goodbye!\n");
+	pr_debug("Goodbye!\n");
 }
+
 module_init(hello_init);
 module_exit(hello_exit);
-
